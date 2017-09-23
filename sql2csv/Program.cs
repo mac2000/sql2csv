ee64e423325d417f98721e2539c63413ad061add
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2017-09-23 10:07:17 +0300

    initial read write

--- sql2csv/Program.cs
+++ sql2csv/Program.cs
@@ -1,13 +1,92 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace sql2csv
 {
 	public class Program
 	{
+		public static ParallelOptions ParallelOptions = new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount / 2 };
+		protected static readonly BlockingCollection<string> Queue = new BlockingCollection<string>();
+		public static long InputRows;
+		public static long OutputRows;
+
 		public static int Main(string[] args)
+		{
+			var (query, output, connectionString) = ParseArgs(args);
+			if (string.IsNullOrEmpty(query) || string.IsNullOrEmpty(output) || string.IsNullOrEmpty(connectionString))
+			{
+				PrintHelpMessage();
+				return 1;
+			}
+
+			var global = Stopwatch.StartNew();
+
+			var read = Task.Run(() =>
+			{
+				var timer = Stopwatch.StartNew();
+				using (var connection = new SqlConnection(connectionString))
+				{
+					using (var command = new SqlCommand(query, connection) { CommandTimeout = 0 })
+					{
+						connection.Open();
+						using (var reader = command.ExecuteReader())
+						{
+							if (reader.HasRows)
+							{
+								while (reader.Read())
+								{
+									var list = new List<string>();
+									for (var i = 0; i < reader.FieldCount; i++)
+									{
+										var val = Regex.Replace(Regex.Replace((reader[i] ?? "").ToString().Trim(), "\\s+", " "), "\"", "\\\"");
+										list.Add("\"" + val + "\"");
+									}
+									Queue.Add(string.Join(",", list));
+									Interlocked.Increment(ref InputRows);
+									if (InputRows % 1000 == 0)
+									{
+										Console.Write($"{InputRows:N0} in {timer.Elapsed}\r");
+									}
+								}
+							}
+							reader.Close();
+						}
+					}
+				}
+				Queue.CompleteAdding();
+				Console.WriteLine();
+				Console.WriteLine($"Got {InputRows:N0} rows from database in {timer.Elapsed}");
+			});
+
+			var write = Task.Run(() =>
+			{
+				var timer = Stopwatch.StartNew();
+				using (var writer = new StreamWriter(output))
+				{
+					foreach (var row in Queue.GetConsumingEnumerable())
+					{
+						writer.WriteLine(row);
+						Interlocked.Increment(ref OutputRows);
+					}
+				}
+				Console.WriteLine($"Done writing {OutputRows:N0} in {timer.Elapsed}");
+			});
+
+			Task.WaitAll(read, write);
+			Console.WriteLine($"Done in {global.Elapsed}");
+
+			return 0;
+		}
+
+		private static (string query, string output, string connectionString) ParseArgs(string[] args)
 		{
 			var options = args.ToDictionary(arg => arg.TrimStart('-').Split('=').FirstOrDefault(), arg => arg.Split('=').LastOrDefault());
 
@@ -24,23 +103,7 @@ namespace sql2csv
 
 			if (!(hasValidInput && hasValidOutput))
 			{
-				Console.WriteLine("Required arguments:");
-				Console.WriteLine();
-				Console.WriteLine("  --query=\"select top 10 * from city\" - required if there is no input argument");
-				Console.WriteLine("  --input=query.sql - required if there is no query argument");
-				Console.WriteLine("  --output=city.csv");
-				Console.WriteLine();
-				Console.WriteLine("Optional arguments:");
-				Console.WriteLine("  --server=localhost");
-				Console.WriteLine("  --database=RabotaUA2");
-				Console.WriteLine("  --username=sa");
-				Console.WriteLine("  --password=password");
-				Console.WriteLine();
-				Console.WriteLine("Usage examples:");
-				Console.WriteLine();
-				Console.WriteLine("sql2csv --query=\"select top 10 * from city\" --output=city.csv");
-				Console.WriteLine();
-				return 1;
+				return ("", "", "");
 			}
 
 			if (string.IsNullOrEmpty(query))
@@ -65,29 +128,27 @@ namespace sql2csv
 				builder.Password = password;
 			}
 
+			return (query, output, builder.ConnectionString);
+		}
 
-
-			using (var connection = new SqlConnection(builder.ConnectionString))
-			{
-				using (var command = new SqlCommand(query, connection) { CommandTimeout = 0 })
-				{
-					connection.Open();
-					using (var reader = command.ExecuteReader())
-					{
-						if (reader.HasRows)
-						{
-							while (reader.Read())
-							{
-								//stats[i] += 1;
-								//Console.Write("{0}\r", string.Join(", ", stats.Select(num => string.Format("{0:N0}", num))));
-							}
-						}
-						reader.Close();
-					}
-				}
-			}
-
-			return 0;
+		private static void PrintHelpMessage()
+		{
+			Console.WriteLine("Required arguments:");
+			Console.WriteLine();
+			Console.WriteLine("  --query=\"select top 10 * from city\" - required if there is no input argument");
+			Console.WriteLine("  --input=query.sql - required if there is no query argument");
+			Console.WriteLine("  --output=city.csv");
+			Console.WriteLine();
+			Console.WriteLine("Optional arguments:");
+			Console.WriteLine("  --server=localhost");
+			Console.WriteLine("  --database=RabotaUA2");
+			Console.WriteLine("  --username=sa");
+			Console.WriteLine("  --password=password");
+			Console.WriteLine();
+			Console.WriteLine("Usage examples:");
+			Console.WriteLine();
+			Console.WriteLine("sql2csv --query=\"select top 10 * from city\" --output=city.csv");
+			Console.WriteLine();
 		}
 	}
 }
