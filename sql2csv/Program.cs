e0f390e98e1dfecf5e87de5ed33858f8858d0cbd
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2017-09-23 08:41:06 +0300

    Add project files.

---
+++ sql2csv/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sql2csv
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
