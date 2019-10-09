b71d38e70a0c5993db591124414f1e00a465fd47
Marchenko Alexandr <marchenko.alexandr@gmail.com>
2019-10-09 10:01:50 +0300

    dotnet core 3 (#2)
    cross platform sql2csv

---
+++ DictionaryExtensions.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace sql2csv
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue val) => dict.ContainsKey(key) && dict[key] != null ? dict[key] : val;
+    }
+}
