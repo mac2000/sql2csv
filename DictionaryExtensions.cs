9704ce780dce544be003c52a9a0025c234939ba5
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2020-06-18 09:43:35 +0300

    nullability check

--- DictionaryExtensions.cs
+++ DictionaryExtensions.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections.Generic;
 
 namespace sql2csv
 {
     public static class DictionaryExtensions
     {
-        public static TValue GetOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue val) => dict.ContainsKey(key) && dict[key] != null ? dict[key] : val;
+        public static TValue GetOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue val) => dict != null && dict.ContainsKey(key) && dict[key] != null ? dict[key] : val;
     }
 }
